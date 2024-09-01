5f3b541465542d0fcefe9ac533e2f681055c0547
Emirhan Kurt <emirhankurtofc@gmail.com>
2024-09-01 14:16:48 +0300

    Developed Footer

--- src/components/Navigation/Item.jsx
+++ src/components/Navigation/Item.jsx
@@ -1,16 +1,42 @@
+import { amplifyClassnamePropWithStaticClassname } from '../../utils';
 import Link from '../Link';
 
-const Item = ({ referTo, displayText, isButton = false, target = '_top' }) => {
-  return (
-    <Link
-      target={target}
-      className="navigation-item"
-      referTo={referTo}
-      isButton={isButton}
-    >
-      {displayText}
-    </Link>
+const Item = ({
+  referTo,
+  displayText,
+  isButton = false,
+  target = '_top',
+  className = '',
+  children
+}) => {
+  const classNameProperty = amplifyClassnamePropWithStaticClassname(
+    'navigation-item',
+    className
   );
+
+  if (displayText)
+    return (
+      <Link
+        target={target}
+        className={classNameProperty}
+        referTo={referTo}
+        isButton={isButton}
+      >
+        {displayText}
+      </Link>
+    );
+
+  if (children)
+    return (
+      <Link
+        target={target}
+        className={classNameProperty}
+        referTo={referTo}
+        isButton={isButton}
+      >
+        {children}
+      </Link>
+    );
 };
 
 export default Item;
